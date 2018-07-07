3a56efa27e6735129f100a0f6b3f8ef388e71bd0
WisON27 <vrm@cin.ufpe.br>
2018-07-07 17:29:09 -0300

    fix web

--- NODE-INTEGRACAO/sample.js
+++ NODE-INTEGRACAO/sample.js
@@ -2,9 +2,9 @@ const publish = require('./publisher')
 const subscribe = require('./subscriber').subscribe
 const subscribeQueue = require('./subscriber').subscribeQueue
 
-publish('bus', 3333) //call 3333, after getAllBus return [ '3333' ]
+publish('bus', '3333') //call 3333, after getAllBus return [ '3333' ]
 
-subscribe('3333', (msg) => { 
+subscribe('3333', (msg) => {
     //return example: msg = '3333;E1;3333;2018-01-22 13:33:56.740;1;1;289257;9113824;NULL;NULL;NULL;NULL;NULL'
     console.log('- Receive:', msg)
 })
@@ -14,7 +14,7 @@ subscribe('3333', (msg) => {
 let allBus = {}
 subscribeQueue('allbus', (msg) => {
     allBus[msg] = ''
-    
+
 })
 const getAllBus = () => {
     return Object.keys(allBus)
--- server/index.js
+++ server/index.js
@@ -2,23 +2,27 @@ const WebSocketServer = require('websocket').server;
 const http = require('http');
 const publish = require('./publisher')
 const subscribe = require('./subscriber').subscribe
-const server = http.createServer(function(request, response) {
-  // process HTTP request. Since we're writing just WebSockets
-  // server we don't have to implement anything.
+const utm = require('utm')
+const server = http.createServer(function (request, response) {
+    // process HTTP request. Since we're writing just WebSockets
+    // server we don't have to implement anything.
 });
 
-server.listen(80, function() { });
+server.listen(8080, function () { });
 
 const wsServer = new WebSocketServer({
     httpServer: server
 });
 
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
     var connection = request.accept(null, request.origin);
-    connection.on('message', function(message) {
+    connection.on('message', function (message) {
+        console.log(message)
+        message = message.utf8Data
         publish('bus', message) //call 3333, after getAllBus return [ '3333' ]
-        
-        subscribe(message, (_row) => { 
+
+        subscribe(message, (_row) => {
+            console.log(_row)
             const row = _row.split(';');
             const _utm = utm.toLatLon(row[6], row[7], 25, 'l');
             row[6] = _utm.latitude;
@@ -27,5 +31,5 @@ wsServer.on('request', function(request) {
         })
     });
 });
-  
+
 
